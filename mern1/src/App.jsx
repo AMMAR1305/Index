d3cb6b2ae2862e8c2a551282b220b4e1202e1259
AMMAR1305 <md.ammar1305@gmail.com>
2025-10-13 15:38:02 +0530

    React

---
+++ mern1/src/App.jsx
@@ -0,0 +1,23 @@
+import  React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is my first React app.</p>
+      <p>Welcome to the world of React!</p>
+      <p>Let's build something amazing together.</p>
+      <p>Happy coding!</p>
+      <p>React makes it easy to create interactive UIs.</p>
+      <p>Enjoy your journey with React!</p>
+      <p>Stay curious and keep learning.</p>
+      <p>Thank you for visiting my React app!</p>
+      <p>Feel free to explore and experiment.</p>
+      <p>React is a powerful library for building web applications.</p>
+      <p>Have fun coding with React!</p>
+      <p>See you next time!</p>
+    </div>
+  )
+}
+
+export default App
